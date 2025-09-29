5724040476554f08edf8d2beacfccda28662638c
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-09-29 09:08:17 +0100

    1.0.1

--- src/main/java/com/benbenlaw/shops/command/BalanceCommand.java
+++ src/main/java/com/benbenlaw/shops/command/BalanceCommand.java
@@ -18,6 +18,7 @@ public class BalanceCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal("player_balance")
                 .then(Commands.literal("add")
+                        .requires(source -> source.hasPermission(4))
                         .then(Commands.argument("player", EntityArgument.player())
                                 .then(Commands.argument("amount", IntegerArgumentType.integer())
                                         .executes(ctx -> addBalance(ctx, EntityArgument.getPlayer(ctx, "player"), IntegerArgumentType.getInteger(ctx, "amount")))
@@ -25,6 +26,7 @@ public class BalanceCommand {
                         )
                 )
                 .then(Commands.literal("remove")
+                        .requires(source -> source.hasPermission(4))
                         .then(Commands.argument("player", EntityArgument.player())
                                 .then(Commands.argument("amount", IntegerArgumentType.integer())
                                         .executes(ctx -> removeBalance(ctx, EntityArgument.getPlayer(ctx, "player"), IntegerArgumentType.getInteger(ctx, "amount")))
@@ -32,9 +34,17 @@ public class BalanceCommand {
                         )
                 )
                 .then(Commands.literal("set")
+                        .requires(source -> source.hasPermission(4))
                         .then(Commands.argument("player", EntityArgument.player())
                                 .then(Commands.argument("amount", IntegerArgumentType.integer())
                                         .executes(ctx -> setBalance(ctx, EntityArgument.getPlayer(ctx, "player"), IntegerArgumentType.getInteger(ctx, "amount")))
+                                )
+                        )
+                )
+                .then(Commands.literal("transfer")
+                        .then(Commands.argument("to_player", EntityArgument.player())
+                                .then(Commands.argument("amount", IntegerArgumentType.integer())
+                                        .executes(ctx -> transferBalance(ctx, EntityArgument.getPlayer(ctx, "to_player"),  IntegerArgumentType.getInteger(ctx, "amount")))
                                 )
                         )
                 )
@@ -65,6 +75,27 @@ public class BalanceCommand {
                 () -> net.minecraft.network.chat.Component.literal("Set " + player.getName().getString() + "'s balance to " + amount), true);
 
         PacketDistributor.sendToPlayer(player, new SyncPlayerBalanceToClient(player.getData(ShopsAttachments.PLAYER_BALANCE.get()).getBalance()));
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int transferBalance(CommandContext<CommandSourceStack> ctx, ServerPlayer toPlayer, int amount) {
+
+       ServerPlayer fromPlayer = ctx.getSource().getPlayer();
+
+        assert fromPlayer != null;
+        if (fromPlayer.getData(ShopsAttachments.PLAYER_BALANCE.get()).getBalance() < amount) {
+            ctx.getSource().sendFailure(net.minecraft.network.chat.Component.literal(fromPlayer.getName().getString() + " does not have enough balance to transfer " + amount));
+            return 0;
+        }
+
+        fromPlayer.getData(ShopsAttachments.PLAYER_BALANCE.get()).subtractBalance(amount);
+        toPlayer.getData(ShopsAttachments.PLAYER_BALANCE.get()).addBalance(amount);
+
+        ctx.getSource().sendSuccess(
+                () -> net.minecraft.network.chat.Component.literal("Transferred " + amount + " from " + fromPlayer.getName().getString() + " to " + toPlayer.getName().getString()), true);
+
+        PacketDistributor.sendToPlayer(fromPlayer, new SyncPlayerBalanceToClient(fromPlayer.getData(ShopsAttachments.PLAYER_BALANCE.get()).getBalance()));
+        PacketDistributor.sendToPlayer(toPlayer, new SyncPlayerBalanceToClient(toPlayer.getData(ShopsAttachments.PLAYER_BALANCE.get()).getBalance()));
         return Command.SINGLE_SUCCESS;
     }
 }
